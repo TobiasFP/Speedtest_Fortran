5e1969c62b2fcee23c15281318a6b8ffc8d4883d
Tobias <tobi.fp@gmail.com>
2021-08-27 15:32:52 +0200

    refactored innerloop to inner_loop

--- speedtest.f90
+++ speedtest.f90
@@ -1,12 +1,12 @@
 program speedtest
     use, intrinsic :: iso_fortran_env
     REAL(real64) :: sum
-    INTEGER :: element, iteration, innerloop, array_length, iterations
+    INTEGER :: element, iteration, inner_loop, array_length, iterations
     REAL(real64), DIMENSION(100000000) :: counter_array
     sum = 0.0
     element = 0
     iteration = 0
-    innerloop = 0
+    inner_loop = 0
     array_length = 100000000
     iterations = 10
 
@@ -15,9 +15,9 @@ program speedtest
         element = element + 1
     end do
     do while (iteration <= iterations)
-        do while (innerloop <= array_length)
-            sum = sum + counter_array(Mod((iteration + innerloop), array_length))
-            innerloop = innerloop + 1
+        do while (inner_loop <= array_length)
+            sum = sum + counter_array(Mod((iteration + inner_loop), array_length))
+            inner_loop = inner_loop + 1
         end do
         iteration = iteration + 1
     end do
